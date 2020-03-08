ab57acffbbc01fe7e9fc735ecdda629553d33121
BicskS <bicskas94@gmail.com>
2020-03-08 19:35:22 +0100

    slick slider
    Took 14 minutes

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,28 +5,51 @@
  */
 
 require('./bootstrap');
+require('slick-carousel/slick/slick.min');
 
-window.Vue = require('vue');
+$(function () {
+    $('.slick-multiple-items').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }]
+    });
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app',
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+    });
 });
--- resources/views/sponsored/list.blade.php
+++ resources/views/sponsored/list.blade.php
@@ -2,18 +2,38 @@
 
 @section('content')
     <div class="container">
-        @foreach($teams as $team)
-            <h2>{!! $team->name !!}</h2>
-            {!! $team->content !!}
-            {!! Html::image($team->getImage(2.77,600),$team->name,['class' => 'img-fluid']) !!}
-        @endforeach
-        <hr>
-        @foreach($riders as $rider)
-            <h2>{!! $rider->name !!}</h2>
-            <h4>{!! $rider->team !!}</h4>
-            <h4>{!! $rider->race !!}</h4>
-            {!! $rider->content !!}
-            {!! Html::image($rider->getImage(2.77,600),$rider->name,['class' => 'img-fluid']) !!}
-        @endforeach
+        <div class="slider-for">
+            @foreach($teams as $team)
+                <div>
+
+                    <h2>{!! $team->name !!}</h2>
+                    {!! $team->content !!}
+                    {!! Html::image($team->getImage(2.77,600),$team->name,['class' => 'img-fluid']) !!}
+                </div>
+            @endforeach
+            @foreach($riders as $rider)
+                <div>
+
+                    <h2>{!! $rider->name !!}</h2>
+                    <h4>{!! $rider->team !!}</h4>
+                    <h4>{!! $rider->race !!}</h4>
+                    {!! $rider->content !!}
+                    {!! Html::image($rider->getImage(2.77,600),$rider->name,['class' => 'img-fluid']) !!}
+                </div>
+            @endforeach
+        </div>
+
+        <div class="slider-nav">
+            @foreach($teams as $team)
+                <div>
+                    {!! Html::image($team->getImage(2.77,600),$team->name,['class' => 'img-fluid']) !!}
+                </div>
+            @endforeach
+            @foreach($riders as $rider)
+                <div>
+                    {!! Html::image($rider->getImage(2.77,600),$rider->name,['class' => 'img-fluid']) !!}
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
