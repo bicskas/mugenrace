39f249bbcd6dbf8a30f8d9b3f5aae42b574db816
BicskS <bicskas94@gmail.com>
2020-03-16 11:30:31 +0100

    homepage overflow
    Took 28 minutes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-100">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-100 @yield('body_class')">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
