aa8d5f264f705997dd3cb4cd192b07c3bcf1fa2b
BicskS <bicskas94@gmail.com>
2020-03-24 09:17:38 +0100

    jcrop javítás
    Took 12 minutes

--- app/Http/Controllers/Admin/RiderController.php
+++ app/Http/Controllers/Admin/RiderController.php
@@ -49,7 +49,7 @@ class RiderController extends TemplateController
         }
 
         $rider->fill($data)->save();
-
+        dd($request->get('_crops'), $rider->image()->cropolas($request->get('_crops', array())));
         $rider->image()->kepfeltoltes($request->file('image'));
         $rider->image()->cropolas($request->get('_crops', array()));
 
